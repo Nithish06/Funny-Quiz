f86c14c3a4e220e73729d35332f5b282ec1aa83c
Nithish06 <nithss0404@gmail.com>
2021-08-14 16:56:42 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+var readline=require('readline-sync');
+var score=0
+var user=readline.question("Welcome to Football Quiz Please Enter your name: ");
+
+
+
+console.log("Welcome ",user + " Let's start with the quiz: ");
+
+var question=[
+    {
+    ques: "Who's the top scorer of Uefa Champions League : ",
+    ans: "cristiano ronaldo"
+  },
+    {
+    ques: "Who is the all time scorer of England premier league? ",
+    ans: "alan shearer"
+  },
+  {
+    ques:"Who has won the Champions League the most times? ",
+    ans: "real radrid"
+  },
+
+  {
+    ques: "who won the premier league most times? ",
+    ans: "manchester united	"
+  },
+
+];
+
+function checkAnswers(data,res){
+  if(data.ans==res.toLowerCase()){
+    score++;
+    console.log("Correct Answer,Your Score = "+ score);
+  }else{
+    score--;
+    console.log("Wrong Answer Your Score = "+ score);
+  }
+}
+
+for(var i=0;i<question.length;i++){
+  var res=readline.question(question[i].ques);
+  checkAnswers(question[i],res);
+}
+
+console.log("Your Final Score is " + score);
